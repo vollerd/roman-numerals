a549a4c3ad2d8134e550212cc26f1428d3e234b4
David Voller <david.voller@gmail.com>
2012-08-31 16:13:11 +0900

    Ruby source

---
+++ roman.rb
@@ -0,0 +1,14 @@
+def roman_to_num(roman)
+	key={"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
+	p = c = n =0
+	roman.each_char do |i|
+		c=key[i]
+		n += p if p>=c
+		n -= p if p<c
+		p = c
+	end
+	n+=c
+end 
+
+puts roman_to_num('MCMLXXVIII')
+
